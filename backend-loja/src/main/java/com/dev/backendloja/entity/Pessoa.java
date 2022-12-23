bceaf3bbb01370a838f9250063ff5c446967aece
Lucas Gomes <lucasgomessilva713@gmail.com>
2022-12-23 15:20:06 -0300

    implementacao da entidade PermissaoPessoaadicionado a PermissaoPessoa na entidade Pessoa e gerando  setPermissaoPessoa manual sem o Lombok

--- backend-loja/src/main/java/com/dev/backendloja/entity/Pessoa.java
+++ backend-loja/src/main/java/com/dev/backendloja/entity/Pessoa.java
@@ -1,18 +1,23 @@
 package com.dev.backendloja.entity;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
@@ -27,6 +32,10 @@ public class Pessoa {
     @JoinColumn(name = "idCidade")
     private Cidade cidade;
 
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE} )
+    @Setter(value = AccessLevel.NONE)    
+    private List<PermissaoPessoa> permissaoPessoas;
+
     private String nome;
     private String cpf;
     private String email;
@@ -39,5 +48,12 @@ public class Pessoa {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+
+    public void setPermissaoPessoas(List<PermissaoPessoa> pp){
+        for (PermissaoPessoa p : pp) {
+            p.setPessoa(this);
+        }
+        this.permissaoPessoas = pp;
+    }
     
 }
